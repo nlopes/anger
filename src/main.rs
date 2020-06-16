934288b9d985977953702a5d751c2681c1a89928
Norberto Lopes <nlopes.ml@gmail.com>
2020-06-16 22:10:15 +0100

    Bump dependencies

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use rand::seq::IteratorRandom;
 use sysinfo::{ProcessExt, SystemExt};
 
 fn main() {
-    let system = sysinfo::System::new();
+    let mut system = sysinfo::System::new_all();
+    system.refresh_processes();
     let mut rng = rand::thread_rng();
 
     let proc = system
-        .get_process_list()
+        .get_processes()
         .iter()
         .filter(|(pid, _)| **pid >= 1024)
         .map(|(_, proc)| proc)
