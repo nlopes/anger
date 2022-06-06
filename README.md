1c6d2737b498b77e9211d2d81e0f3c3bfec5af00
Norberto Lopes <nlopes.ml@gmail.com>
2022-06-06 20:25:10 +0100

    Bump deps

---
+++ README.md
@@ -0,0 +1,5 @@
+# anger
+
+This is a silly tool that randomly kills a process with a pid >= 1024.
+
+I built it to remind me how pointless it is to do something in anger.
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,22 @@ use std::{thread, time};
 
 use rand;
 use rand::seq::IteratorRandom;
-use sysinfo::{ProcessExt, SystemExt};
+use sysinfo::{Pid, PidExt, ProcessExt, System, SystemExt};
 
 fn main() {
-    let mut system = sysinfo::System::new_all();
+    let mut system = System::new_all();
     system.refresh_processes();
     let mut rng = rand::thread_rng();
 
     let proc = system
-        .get_processes()
+        .processes()
         .iter()
-        .filter(|(pid, _)| **pid >= 1024)
+        .filter(|(pid, _)| **pid >= Pid::from_u32(1024))
         .map(|(_, proc)| proc)
         .choose(&mut rng)
         .unwrap();
 
     println!("I'm angry, gonna kill {}", proc.name());
     thread::sleep(time::Duration::from_secs(5));
-    proc.kill(sysinfo::Signal::Kill);
+    proc.kill();
 }
